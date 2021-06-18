9b6516f1e49271eac3a17991967e884888c406b5
Eben Stine <ebenstine@gmail.com>
2021-06-18 11:08:55 -0500

    2nd commit thru 18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -92,3 +92,5 @@ switch (mostPets) {
 
 console.log(mostPets);
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+var RoadTripReady = (adventurous) ? 'Adventures are great!' : 'How about we stay home?';
+console.log(RoadTripReady);
